5d9ea1cc32faa2f5e894544c82c6da3b855c0fc6
Takashi Hiraoka <hiraokashi@gmail.com>
2015-04-19 21:24:45 +0900

    remove assets precomipile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -7,6 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-Rails.application.config.assets.precompile += %w( libs/modernizr.custom.js )
+#Rails.application.config.assets.precompile += %w()
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 #Rails.application.config.assets.precompile += %w(libs/modernizr.custom.js libs/jquery-1.11.2.min.js libs/jquery.easing.1.3.js plugins/bootstrap.min.js #plugins/jquery.touchSwipe.min.js   plugins/jquery.placeholder.js plugins/icheck.min.js plugins/jquery.validate.min.js plugins/gallery.js #plugins/jquery.fitvids.js plugins/jquery.bxslider.min.js plugins/chart.js plugins/waypoints.min.js plugins/smoothscroll.js landing1.js)
