c0b13778e5f8807d51984f7db7efa1e09c8f64bb
Takashi Hiraoka <hiraokashi@gmail.com>
2015-04-19 12:00:53 +0900

    add baby trimmed

--- app/assets/javascripts/landing1.js
+++ app/assets/javascripts/landing1.js
@@ -27,7 +27,7 @@ $(document).ready(function(e) {
 		adaptiveHeight: true,
 		controls: false,
 		video: true,
-		touchEnabled: true
+		touchEnabled: false
 	});
 	
 	/*Vertically Center Side Navigation*/
