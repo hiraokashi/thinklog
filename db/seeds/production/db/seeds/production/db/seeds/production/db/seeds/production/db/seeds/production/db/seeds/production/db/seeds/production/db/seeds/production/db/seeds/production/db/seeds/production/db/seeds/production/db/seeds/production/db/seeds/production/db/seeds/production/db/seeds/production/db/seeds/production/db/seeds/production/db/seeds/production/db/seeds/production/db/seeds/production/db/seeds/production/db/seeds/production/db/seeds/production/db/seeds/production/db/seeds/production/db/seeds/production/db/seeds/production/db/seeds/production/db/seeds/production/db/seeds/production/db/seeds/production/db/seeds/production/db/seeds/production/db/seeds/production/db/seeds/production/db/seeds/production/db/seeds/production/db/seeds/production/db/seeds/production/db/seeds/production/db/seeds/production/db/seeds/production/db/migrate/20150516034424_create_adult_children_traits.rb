89d7acd3913e7027d39fc521b29307e4cd6bb3f7
Takashi Hiraoka <hiraokashi@gmail.com>
2015-05-16 13:59:56 +0900

    db/productionないに再帰的に作られていたファイルを削除

--- db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/seeds/production/db/migrate/20150516034424_create_adult_children_traits.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdultChildrenTraits < ActiveRecord::Migration
-  def change
-    create_table :adult_children_traits do |t|
-      t.text :contents
-
-      t.timestamps null: false
-    end
-  end
-end
